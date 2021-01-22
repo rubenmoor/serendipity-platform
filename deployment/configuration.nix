a86ac0eb2d3c5e4960bbc1eb57172d1a74c5c465
Ruben Moor <ruben.moor@gmail.com>
2021-01-21 23:07:34 -0500

    nixops: works

--- deployment/configuration.nix
+++ deployment/configuration.nix
@@ -1,5 +1,85 @@
-{ modulesPath, ... }: {
-  imports = [ "${modulesPath}/virtualisation/amazon-image.nix" ];
-  ec2.hvm = true;
-  networking.hostName = "serendipity";
+let
+  region = "eu-central-1";
+  accessKeyId = "rubm-xmg";
+  backend = (import ./../default.nix { }).ghc.backend;
+in {
+  main = { resources, pkgs, ... }: {
+    networking.hostName = "serendipity";
+    networking.firewall.allowedTCPPorts = [ 22 80 443 ];
+    deployment = {
+      targetEnv = "ec2";
+      ec2 = {
+        inherit region accessKeyId;
+        ebsInitialRootDiskSize = 30;
+        instanceType = "t2.micro";
+        keyPair = resources.ec2KeyPairs.keys;
+        elasticIPv4 = resources.elasticIPs.serendipity;
+        securityGroups = [ resources.ec2SecurityGroups.serendipityGroup ];
+      };
+    };
+    users.extraUsers.root.shell = pkgs.zsh;
+    programs = {
+      zsh = {
+        enable = true;
+        interactiveShellInit = ''
+          export TERM=rxvt
+        '';
+        ohMyZsh = {
+          enable = true;
+          theme = "robbyrussell";
+        };
+        syntaxHighlighting.enable = true;
+      };
+    };
+    services = {
+      mysql = {
+        enable = true;
+        package = pkgs.mariadb;
+        initialDatabases = [ { name="serendipity"; } ];
+        ensureUsers = [
+          {
+            name = "root";
+            ensurePermissions = {
+              "serendipity.*" = "ALL PRIVILEGES";
+            };
+          }
+        ];
+        settings.mysqld = {
+          character-set-server = "utf8mb4";
+          collation-server = "utf8mb4_unicode_ci";
+        };
+      };
+    };
+    systemd.services = {
+      backend = {
+        description = "Serendipity Platform server";
+        wantedBy = [ "multi-user.target" ];
+        after = [ "network.target" ];
+        serviceConfig = {
+          ExecStart = ''
+            ${backend}/bin/backend \
+              --mysql-user root \
+              --mysql-database serendipity \
+              --port 80 \
+              --url https://podcast-staging.rubenmoor.net \
+              --media-directory /home/www-data/media
+          '';
+        };
+      };
+    };
+  };
+
+  resources = {
+    ec2KeyPairs.keys = { inherit region accessKeyId; };
+    ec2SecurityGroups.serendipityGroup = {
+      inherit region accessKeyId;
+      name = "serendipity-group";
+      description = "serendipity.works security group";
+      rules = [
+        { fromPort = 22; toPort = 22; sourceIp = "0.0.0.0/0"; }
+        { fromPort = 80; toPort = 80; sourceIp = "0.0.0.0/0"; }
+      ];
+    };
+    elasticIPs.serendipity = { inherit region accessKeyId; };
+  };
 }
--- deployment/server.nix
+++
@@ -1,6 +0,0 @@
-let
-  nixos = import <nixpkgs/nixos> {
-    configuration = import ./configuration.nix;
-  };
-in
-  nixos.system
