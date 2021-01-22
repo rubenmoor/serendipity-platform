1f73af4fdaaa398e83e30d16d995698b2f05b348
Ruben Moor <ruben.moor@gmail.com>
2021-01-22 16:58:44 -0500

    servant reflex as submodule to workaournd nix build problem

--- default.nix
+++ default.nix
@@ -6,23 +6,23 @@
     common = ./common;
     backend = ./backend;
     frontend = ./frontend;
-    gerippe = ../../gerippe;
   };
 
   shells = {
-    ghc = ["common" "backend" "frontend"];
+    ghc = ["common" "backend"];
     ghcjs = ["common" "frontend"];
   };
   shellToolOverrides = ghc: super: {
     haskell-language-server = unstable.haskell-language-server;
   };
   overrides = self: super: {
-    gerippe = self.callPackage ../../gerippe {};
-    servant-reflex = pkgs.fetchFromGitHub {
-      owner = "imalsogreg";
-      repo = "servant-reflex";
-      rev = "master";
-      sha256 = "0issnp95rnji3v9qifr0brypxsvmjkzanfq5lscj68lynnjv37g0";
-    };
+    gerippe = self.callPackage ./gerippe {};
+    servant-reflex = self.callPackage ./servant-reflex {};
+    # servant-reflex = pkgs.fetchFromGitHub {
+    #   owner = "imalsogreg";
+    #   repo = "servant-reflex";
+    #   rev = "1109043c8437a375fc00bb48792e8dc377a84494";
+    #   sha256 = "0issnp95rnji3v9qifr0brypxsvmjkzanfq5lscj68lynnjv37g0";
+    # };
   };
 })
